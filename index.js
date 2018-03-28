a44b9f8790635407b1b5ca1f9732863c412c49a5
HoangChu96 <chutuhoang@gmail.com>
2018-03-28 12:37:21 +0700

    add project galery photos

---
+++ index.js
@@ -0,0 +1,12 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+import { StackNavigator } from 'react-navigation'
+import ImageBrowser from './ImageBrowser'
+
+const Navigation = StackNavigator({
+  App: { screen: App },
+  ImageBrowser: { screen: ImageBrowser }
+});
+
+AppRegistry.registerComponent('rnCameraRoll', () => Navigation);
